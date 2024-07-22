a52ae51074285bb41761881ecb53071522a9b470
Phil Teigne <philippe.teigne@gmail.com>
2024-07-22 18:24:04 -0400

    simple test Home screen

--- yesChefMobile/app/(tabs)/index.tsx
+++ yesChefMobile/app/(tabs)/index.tsx
@@ -1,52 +1,44 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Text, View } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+// -----
+import React from 'react';
+
+// import './assets/fonts/fonts.css';
+
+// import CreateRecipe from './Components/CreateRecipe.jsx';
+// import ButtonAppBar from './Components/Navigation';
+// import { applicationContext } from './hooks/applicationContext';
+// import useApplicationData from './hooks/customHook';
+// import HomePage from './Components/HomePage.jsx';
+// import SignInSide from './Components/Login.jsx';
+// import ViewRecipe from './Components/ViewRecipe.jsx';
+// import ChatBubble from './Components/ChatBubble.jsx';
+// import ChatModal from './Components/ChatModal.jsx';
+// import PrivateRoute from './Components/PrivateRoute.jsx';
+
+// -----
+
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">How are you doing Raquel!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  // <applicationContext.Provider value={{state, dispatch}}>
+    <View>
+      <Text style={styles.white}>Testing</Text>
+      {/* <ButtonAppBar />
+      <ChatBubble /> */}
+      {/* {state.chatModalOpen && <ChatModal />} */}
+      <View>
+        {/* <Route path='/' exact component={HomePage}/>
+        <Route path='/login' component={SignInSide} />
+        <PrivateRoute path="/create-recipe" component={CreateRecipe} auth={state.isLoggedIn} />
+        <PrivateRoute path="/view-recipe" component={ViewRecipe} auth={state.isLoggedIn} /> */}
+      </View>
+    </View>
+  // </applicationContext.Provider>
   );
 }
 
@@ -67,4 +59,7 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  white: {
+    color: 'white'
+  }
 });
