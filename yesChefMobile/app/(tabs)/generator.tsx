1e50ef8855e91193df319e0664bf580d27b5b4a5
Cody <codylau6233@hotmail.com>
2024-07-27 12:52:34 -0700

    Add state management to generator.tsx.

--- yesChefMobile/app/(tabs)/generator.tsx
+++ yesChefMobile/app/(tabs)/generator.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
@@ -6,28 +6,54 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
-export default function Generator() {
-  const handleGenerate = () => {
-    // Logic to generate recipe
-    console.log('Generate button pressed');
-  };
+const Generator: React.FC = () => {
+  const [recipeTags, setRecipeTags] = useState<string>('');
+  const [recipeFocus, setRecipeFocus] = useState<string>('');
+  const [recipeAvoid, setRecipeAvoid] = useState<string>('');
+
+  const handleSubmit = () => {
+    const recipeRequest = {
+      // allIngredients: state.ingredientList,
+      recipeTags: recipeTags,
+      recipeFocus: recipeFocus,
+      recipeAvoid: recipeAvoid,
+    }
+
+    // Dispatch action to add ingredient
+    // dispatch({ type: "REQUEST_RECIPE", payload: recipeRequest });
+
+    // Dispatch action to set temporary parameter input for recipe regeneration
+    // dispatch({ type: "SET_TEMP_PARAMETER_INPUT", payload: recipeRequest })
+    // Clear input values
+    setRecipeTags('');
+    setRecipeFocus('');
+    setRecipeAvoid('');
+    console.log('submitted');
+    
+  }
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>Recipe Generator</Text>
       <TextInput
+        value={recipeTags}
+        onChangeText={setRecipeTags}
         style={styles.input}
         placeholder="What should we cook tonight?"
       />
       <TextInput
+        value={recipeFocus}
+        onChangeText={setRecipeFocus}
         style={styles.input}
         placeholder="What ingredients should we use?"
       />
       <TextInput
+        value={recipeAvoid}
+        onChangeText={setRecipeAvoid}
         style={styles.input}
         placeholder="What ingredients should we avoid?"
       />
-      <Button title="Generate Recipe" onPress={handleGenerate} />
+      <Button title="Generate Recipe" onPress={handleSubmit} />
     </ScrollView>
   );
 }
@@ -53,3 +79,5 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
 });
+
+export default Generator;
