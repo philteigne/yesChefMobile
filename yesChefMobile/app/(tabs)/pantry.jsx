1b08a305b442259eccad6eaee2658d65dcbc7165
Phil Teigne <philippe.teigne@gmail.com>
2024-08-21 15:39:56 -0400

    implemented basic styling of component

--- yesChefMobile/app/(tabs)/pantry.jsx
+++ yesChefMobile/app/(tabs)/pantry.jsx
@@ -3,79 +3,47 @@ import { Image, StyleSheet, Platform, Text, View, Pressable } from 'react-native
 import { IngredientList } from '@/components/IngredientList';
 import { AddIngredient } from '@/components/AddIngredient';
 
-
 // -----
 import React from 'react';
 
 // -----
 
+const styles = StyleSheet.create({
+  appBackground: {
+    backgroundColor: '#f3f1ed',
+    marginBottom: '200px'
+  },
+  sectionContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  sectionHeader: {
+    color: '#d89821'
+  },
+});
+
 export default function HomeScreen() {
   return (
-  // <applicationContext.Provider value={{state, dispatch}}>
-    <View
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        padding: 20,
-      }}
-    >
-      <View
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between'
-        }}
-      >
-        <Text style={styles.white}>&#8226; pantry</Text>
+    <View style={styles.appBackground}>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionHeader}>&#8226; pantry</Text>
 
         <Pressable
-          style={{
-            width: 10
-          }}
           onPress={() => {
             console.log('You tapped the button!');
           }}
         >
-          <Text style={styles.white}>O</Text>
+          <Text>O</Text>
         </Pressable>
       </View>
 
       <IngredientList />
 
-      <Text
-        style={[
-          styles.white,
-          {
-          marginTop: 2,
-          marginBottom: 0.5,
-          }
-        ]}
-      >
+      <Text style={styles.sectionHeader}>
         &#8226; add an ingredient
       </Text>
       <AddIngredient />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-  white: {
-    color: 'white'
-  },
-});
--- yesChefMobile/components/IngredientList.jsx
+++ yesChefMobile/components/IngredientList.jsx
@@ -2,28 +2,41 @@ import { useContext } from 'react';
 import { applicationContext } from '@/hooks/applicationContext';
 import { StyleSheet, View, FlatList, Text } from 'react-native';
 
+const styles = StyleSheet.create({
+  ingredientList: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    height: '50%',
+    overflow: 'scroll'
+  },
+  ingredientListItem: {
+    color: '#24231e',
+    fontWeight: 'bold',
+    border: 'solid 2px',
+    borderRadius: '5px',
+    padding: '3px',
+    margin: '3px',
+    fontSize: '12pt',
+    width: '45%',
+    overflow: 'hidden',
+  },
+  sectionHeader: {
+    color: '#d89821'
+  },
+});
+
 export function IngredientList() {
 
   const { state } = useContext(applicationContext)
 
   return (
-    <View
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        padding: 20,
-      }}
-    >
+    <View style={styles.ingredientList}>
       <FlatList
         data={state.ingredientList}
-        renderItem={({item}) => <Text style={styles.white}>{item.name}</Text>}
+        renderItem={({item}) => <Text style={styles.ingredientListItem}>{item.name}</Text>}
       />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  white: {
-    color: 'white'
-  },
-});
